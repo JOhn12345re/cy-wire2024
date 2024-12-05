903f1b9f0069aad94fbd22a637f8eda9cdb910eb
RimeLahkakCY <166617045+RimeLahkakCY@users.noreply.github.com>
2024-12-05 10:48:32 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Appele du fichier.h "avl_station.h" du programme "avl_station.c"
 #include <avl_station.h>
 
 
@@ -8,7 +9,7 @@ int main(int argc, char ** argv){
 FILE* f=NULL;
 Station *station=NULL;
 int *h;
-//argv[1]
+//argv[1] f.txt
 f=fopen("","r+");
 
 if (f == NULL) {
@@ -17,19 +18,24 @@ if (f == NULL) {
 }
 
 char ligne[1000];
-Arbre *AVL = NULL; // Initialisation de la racine de l'AVL à NULL
 
+// Initialisation de la racine de l'AVL à NULL
+Arbre *AVL = NULL; 
+
+//Création de AVL
 while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
 	station=creerStation(f);
         // Insérer les données dans l'AVL
 	AVL = insertionAVL(AVL,station,h);
 }
 
+//Vérifier si l'arbre est bien un AVL
 if(estAVL(AVL)==0){
 	printf("structure AVL incorect");
 	return 1;
 }
 
+//Création du fichier.txt de sortie
 ecrireStation(AVL);
 
 return 0;
