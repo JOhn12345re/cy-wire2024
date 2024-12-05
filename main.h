6e55070a2d5f5ff62fde9a03ea96839000027c46
TNorah <168777016+TNorah@users.noreply.github.com>
2024-12-05 10:09:11 +0100

    Add files via upload

---
+++ main.h
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct station{
+	int id;
+	int capacite;
+	int conso;
+}Station;
+
+typedef struct arbre{
+	Station s;
+	int eq;
+	struct arbre* fg;
+	struct arbre* fd;
+}Arbre;
+
+
+int testSiFichierVide(FILE *fichier);
+Station *creerStation(File *fichier);
+Arbre *creerAVL(Station s);
+int estVide(Arbre *a);
+int estFeuille(Arbre *a);
+int existeFilsGauche(Arbre *a);
+int existeFilsDroite(Arbre *a);
+int hauteur(Arbre *a);
+int equilibre(Arbre *a);
+int ming(int a, int b, int c);
+int mind(int a, int b);
+int maxd(int a, int b, int c);
+Arbre *RotationGauche(Arbre *a);
+Arbre *RotationDroite(Arbre *a);
+Arbre *DblRotationGauche(Arbre *a);
+Arbre *DblRotationDroite(Arbre *a);
+Arbre *equilibreAVL(Arbre *a);
+Arbre *insertionAVL(Arbre *a, Station stat, int *h);
+void infixe(Arbre *a);
+int verifFilsDroit(Arbre *a, int min);
+int verifFilsGauche(Arbre *a, int max);
+int estABR(Arbre *a);
+int estAVL(Arbre *a);
+int somme(Arbre *a);
+void ecrireStation(Arbre *a);
