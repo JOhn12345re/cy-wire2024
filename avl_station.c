2a8064b06e950162f1db95b7077aa2ab8fab5547
RimeLahkakCY <166617045+RimeLahkakCY@users.noreply.github.com>
2024-12-05 10:10:33 +0100

    Update avl_station.c

--- avl_station.c
+++ avl_station.c
@@ -345,34 +345,4 @@ void ecrireStation(Arbre *a){
 	}
 	fclose(fichier);
 }
-
-int main(int argc, char ** argv){
-FILE* f=NULL;
-Station *station=NULL;
-int *h;
-
-f=fopen("","r+");
-
-if (f == NULL) {
-	printf("Impossible d'ouvrir le fichier.\n");
-	return 1;
-}
-
-char ligne[1000];
-Arbre *AVL = NULL; // Initialisation de la racine de l'AVL à NULL
-
-while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
-	station=creerStation(f);
-        // Insérer les données dans l'AVL
-	AVL = insertionAVL(AVL,station,h);
-}
-
-if(estAVL(AVL)==0){
-	printf("structure AVL incorect");
-	return 2;
-}
-
-ecrireStation(AVL);
-
-return 0;
 }
