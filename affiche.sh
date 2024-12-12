ff9176c00759977e10e46d536ede628d0360e312
TNorah <168777016+TNorah@users.noreply.github.com>
2024-12-12 10:44:39 +0100

    Update affiche.sh

--- affiche.sh
+++ affiche.sh
@@ -2,6 +2,7 @@
 
 #code couleur shell http://www.tux-planet.fr/les-codes-de-couleurs-en-bash/ Jaune: \033[1;33m \033[0m
 #ASCII art https://www.asciiart.eu/buildings-and-places/cities
+#https://www.math.univ-paris13.fr/~rousselin/fichiers/enseignement/shell/couleurs_shell.pdf
 
 
 echo -e "            |   _   _
