a260ec4af14e54a4f9f24de7f4f5ca8264e8d173
TNorah <168777016+TNorah@users.noreply.github.com>
2024-12-12 09:08:37 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
-
-
 //Appele du fichier.h "avl_station.h" du programme "avl_station.c"
 #include "avl_station.h"
 
 
 int main(int argc, char ** argv){
 FILE* f=NULL;
-Station *station=NULL;
+Station s;
 int h;
+int n;
 //argv[1] f.txt
 f=fopen(argv[1],"r+");
 
@@ -21,12 +20,29 @@ char ligne[1000];
 // Initialisation de la racine de l'AVL à NULL
 Arbre *AVL = NULL; 
 
+char c;
+while((c = fgetc(f)) != EOF){
+        if(c == '\n'){
+            n++;
+            printf("nb = %d\n",n);
+        }
+    }
+rewind(f);
+
+
 //Création de AVL
-while (fgets(ligne, sizeof(ligne), f) != NULL) {
-	station=creerStation(f);
+for(int i = 0; i<n; i++){
+	fscanf(f, "%d %ld %ld\n", &s.id, &s.capacite,&s.conso);
+        printf("%d %ld %ld\n", s.id, s.capacite,s.conso);
+        //printf("Erreur de lecture de la ligne : %s\n", ligne);
         // Insérer les données dans l'AVL
-	AVL = insertionAVL(AVL,*station,&h);
+       
+	AVL = insertionAVL(AVL,s,&h);
+	
+
 }
+fclose(f);
+
 
 //Vérifier si l'arbre est bien un AVL
 if(estAVL(AVL)==0){
