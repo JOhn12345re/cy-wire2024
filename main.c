d36d64f5225c795284ef5de30422a92e7045fc9f
RimeLahkakCY <166617045+RimeLahkakCY@users.noreply.github.com>
2024-12-12 11:51:58 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,62 +3,60 @@
 
 
 int main(int argc, char ** argv){
+
 FILE* f=NULL;
 Station s;
+
 int h;
-int n;
-//argv[1] f.txt
+int ligne;
+char c;
+
+// Ouverture du fichier en parametre
 f=fopen(argv[1],"r+");
 
+// Message d'erreur si le fichier est NULL
 if (f == NULL) {
-	printf("Impossible d'ouvrir le fichier.\n");
+	printf("\nImpossible d'ouvrir le fichier.\n");
 	return 1;
 }
-
-char ligne[1000];
 
 // Initialisation de la racine de l'AVL à NULL
 Arbre *AVL = NULL; 
 
-char c;
+// Compte le nombre de ligne du fichier f
 while((c = fgetc(f)) != EOF){
         if(c == '\n'){
-            n++;
-            printf("nb = %d\n",n);
+            ligne++;
         }
     }
 rewind(f);
 
 
 //Création de AVL
-for(int i = 0; i<n; i++){
-	fscanf(f, "%d %ld %ld\n", &s.id, &s.capacite,&s.conso);
-        printf("%d %ld %ld\n", s.id, s.capacite,s.conso);
-        //printf("Erreur de lecture de la ligne : %s\n", ligne);
+for(int i = 0; i<ligne; i++){
+//printf("Erreur de lecture de la ligne : %s\n", ligne);
+	if(fscanf(f, "%d %ld %ld\n", &s.id, &s.capacite,&s.conso)!=3){
+		printf("\nNombre de variable incorrect\n");
+		return 1;
+	}
+        
         // Insérer les données dans l'AVL
-       
 	AVL = insertionAVL(AVL,s,&h);
-	
-
 }
 fclose(f);
 
-printf("\n");
-infixe(AVL);
-printf("\n");
-
 //Vérifier si l'arbre est bien un AVL
-
 if(estAVL(AVL)==0){
-	printf("structure AVL incorect");
+	printf("\nStructure AVL incorect\n");
 	return 1;
 }
 
 //Création du fichier.txt de sortie
-FILE *fichier = fopen("TEST.txt","w");
+FILE *fichier = fopen("resultat.txt","w");
 //Premiére ligne
-//Données: identifiants, capacité, consomation totale et production
-fprintf(fichier, "id:capacite:conso_ttl:production\n");
+//Données: identifiants, capacité et consomation totale
+fprintf(fichier, "id:capacite:conso_ttl\n");
+//Fichier de sortie des résultats
 ecrireStation(AVL,fichier);
 fclose(fichier);
 
