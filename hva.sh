f529114b418ad96b8cdf897e4d8a54e8e74565a0
TNorah <168777016+TNorah@users.noreply.github.com>
2024-12-21 16:36:37 +0100

    Delete hva.sh

--- hva.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Filtrage des lignes non vides et sélection des colonnes avec `awk`, remplacement des traits par des 0
-awk -F";" '{if ($3 != "-" && $4 == "-" && $6 == "-") {gsub("-", "0", $3); gsub("-", "0", $7); gsub("-", "0", $8); print $3, $7, $8}}' data.csv > hva.txt
-
