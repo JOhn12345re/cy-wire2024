c2a003ca5f4a174c271610e5b1752cbde6c41917
TNorah <168777016+TNorah@users.noreply.github.com>
2024-12-21 16:36:55 +0100

    Delete hvb.sh

--- hvb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Filtrage des lignes non vides, remplacement des traits par des 0 et sélection des colonnes avec `awk`
-awk -F";" '{if ($2 != "-" && $3 == "-" && $4 == "-" && $6 == "-") {gsub("-", "0", $2); gsub("-", "0", $7); gsub("-", "0", $8); print $2, $7, $8}}' data.csv > hvb.txt
-
