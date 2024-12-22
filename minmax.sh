b5189b3cfad210bf1bdd6129967f59e7e4c0671b
JOhn12345re <153006641+JOhn12345re@users.noreply.github.com>
2024-12-22 11:17:03 +0100

    Delete minmax.sh

--- minmax.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $# -lt 2 ]]; then
-	cat $1 | sort -n -k3 > "sort.txt"
-	head "sort.txt" > "head.txt" 
-	tail "sort.txt" > "tail.txt"
-	cat "tail.txt" "head.txt" > "lv_minmax.txt"
-fi
